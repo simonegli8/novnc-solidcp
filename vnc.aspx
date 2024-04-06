2d40803f2737623f33639a616047994f012359a8
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-04-06 23:33:40 +0200

    Added vnc.apsx and msbuild project file

---
+++ vnc.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="vnc.aspx.cs" Inherits="SolidCP.Providers.Virtualization.NoVNC.vnc" %>
+
+<%= System.IO.File.ReadAllText(Server.MapPath("~/novnc/vnc.html")) %>
+
+<head runat="server" visible="false" />
---
+++ vnc.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SolidCP.Providers.Virtualization.NoVNC
+{
+	public partial class vnc : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
